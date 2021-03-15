e32aef7cbaf39b7fb9cfcb0987f6e7b4dfdd6cf7
paqu <pawel.kusz1993@gmail.com>
2021-03-15 23:00:21 +0100

    Implement adding & removing contact

--- contacts/model.py
+++ contacts/model.py
@@ -11,6 +11,22 @@ class ContactsModel:
     def __init__(self):
         self.model = self._createModel()
 
+    def addContact(self, data):
+        """Add a contact to the database."""
+        rows = self.model.rowCount()
+        self.model.insertRows(rows, 1)
+        for column, field in enumerate(data):
+            self.model.setData(self.model.index(rows, column + 1), field)
+
+        self.model.submitAll()
+        self.model.select()
+
+    def deleteContact(self, row):
+        """Remove a contact from the database."""
+        self.model.removeRow(row)
+        self.model.submitAll()
+        self.model.select()
+
     @staticmethod
     def _createModel():
         """Create and set up the model."""
