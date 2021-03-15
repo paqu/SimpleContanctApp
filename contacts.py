ee47a46610715861880268aa66e1418f1022036c
paqu <pawel.kusz1993@gmail.com>
2021-03-15 21:11:38 +0100

    Create minimal GUI application

---
+++ contacts.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# contacts_project/contacts.py
+
+"""This module provides RP Contacts entry point script."""
+
+from contacts.main import main
+
+if __name__ == "__main__":
+    main()
